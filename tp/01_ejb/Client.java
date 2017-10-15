2acabc10eb813d21147aa87d17cd475c6a060cb3
Chouki Tibermacine <tibermacin@lirmm.fr>
2017-10-15 17:34:19 +0200

    Ajout des sources pour le TP1

--- tp/01_ejb/Client.java
+++ tp/01_ejb/Client.java
@@ -7,7 +7,7 @@ import javax.naming.NamingException;
 
 public class Client {
     public static void main(String[] args) throws NamingException {
-        Converter converter = (Converter) InitialContext.doLookup("java:global/2017_2018_TP1/2017_2018_TP1-ejb/ConverterBean");
+        Converter converter = (Converter) InitialContext.doLookup("java:global/Converter/Converter-ejb/ConverterBean");
         System.out.print("Entrer le montant à convertir : ");
         Scanner sc = new Scanner(System.in);
         float montant = sc.nextFloat();
